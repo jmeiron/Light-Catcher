0e8186a6baa504b436cd28dcc5582aa1066f3c14
jmeiron <79291764+jmeiron@users.noreply.github.com>
2021-03-21 14:17:20 -0500

    Rough Draft
    The code runs but has bugs

---
+++ level-Five.js
@@ -0,0 +1,60 @@
+let one = document.getElementById('one');
+let two = document.getElementById('two');
+let three = document.getElementById('three');
+let four = document.getElementById('four');
+let five = document.getElementById('five');
+let six = document.getElementById('six');
+let seven = document.getElementById('seven');
+let eight = document.getElementById('eight');
+let nine = document.getElementById('nine');
+let ten = document.getElementById('ten');
+let eleven = document.getElementById('eleven');
+let twelve = document.getElementById('twelve');
+let notSeen = document.getElementById('notSeen');
+let click = document.getElementById('click');
+let level = document.getElementById('level');
+let lightArr = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, notSeen]; 
+
+let i = 0;
+
+function returnColor() {
+    setTimeout(function() {
+       lightArr[i - 1].style.backgroundColor = 'black'; 
+    }, 40)
+};
+
+function startGame() {
+    setTimeout(function(){
+        lightArr[i].style.backgroundColor = 'red';
+        i++;
+        if(i < 13){
+            returnColor();
+            startGame();}
+            if(i === 13){
+        i -= 13;
+        startGame();  
+    }  
+   }, 40)     
+};
+
+function advanceRound() {
+window.location.href = 'index.html'
+window.alert('Congrats You Did it')
+}
+
+function loseGame() {
+window.location.href = 'index.html'
+window.alert('You Lost! Try Again')
+}
+
+function checkNum() {
+    if(i === 5){
+       advanceRound(); 
+    }else{
+        loseGame();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', startGame);
+
+click.addEventListener('mousedown', checkNum);
